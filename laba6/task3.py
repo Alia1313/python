5cf348ddb22f4dac58c094f55fdbfc025e202139
Alia <aliya..yarkayeva@bk.ru>
2024-11-10 17:38:38 +0200

    laba6

---
+++ laba6/task3.py
@@ -0,0 +1,6 @@
+import xmltodict
+
+with open('ex_3.xml', encoding="windows-1251") as rawXml:
+    xmlDict = xmltodict.parse(rawXml.read(), encoding="windows-1251")
+    for product in xmlDict["Файл"]["Документ"]["ТаблСчФакт"]["СведТов"]:
+        print(f'Товар: {product["@НаимТов"]}, Количество: {product["@КолТов"]}, Цена: {product["@ЦенаТов"]}')
