14ec8f28a10eb3b82ab66ea156c0cb72e86d5542
Alia1313 <120978661+Alia1313@users.noreply.github.com>
2024-11-10 17:46:50 +0200

    laba6

--- laba6/task3.py
+++ laba6/task3.py
@@ -1,6 +1,17 @@
 import xmltodict
 
-with open('ex_3.xml', encoding="windows-1251") as rawXml:
-    xmlDict = xmltodict.parse(rawXml.read(), encoding="windows-1251")
-    for product in xmlDict["Файл"]["Документ"]["ТаблСчФакт"]["СведТов"]:
-        print(f'Товар: {product["@НаимТов"]}, Количество: {product["@КолТов"]}, Цена: {product["@ЦенаТов"]}')
+def process_xml(input_file):
+    with open(input_file, encoding="windows-1251") as rawXml:
+        xml_dict = xmltodict.parse(rawXml.read(), encoding="windows-1251")
+        
+        products = xml_dict["Файл"]["Документ"]["ТаблСчФакт"]["СведТов"]
+        
+        for product in products:
+            name = product.get("@НаимТов")
+            quantity = product.get("@КолТов")
+            price = product.get("@ЦенаТов")
+            
+            print(f'Товар: {name}, Количество: {quantity}, Цена: {price}')
+
+if __name__ == "__main__":
+    process_xml('ex_3.xml')
